f411705516a5a82a988acec1c24037863fe08b30
Kaustubh-790 <kaustubhsharma434@gmail.com>
2025-10-23 23:12:51 +0530

    add: accordion

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,19 @@
 import { motion } from "framer-motion";
+import Accordion from "../components/Accordion";
+import img1 from "../../public/1.png";
+import img2 from "../../public/2.png";
+
+const images = [
+  { header: "img1", image: img1, text: "img1 desc" },
+  { header: "img2", image: img2, text: "img2 desc" },
+];
 
 export default function Home() {
   return (
     <div className="text-center mt-10 space-y-10">
-      <motion.h1
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.7 }}
-        className="text-4xl md:text-5xl font-bold"
-      >
-        Track. Discover. <span className="text-accent-pink">Love</span> again.
-      </motion.h1>
-
-      <motion.img
-        src="https://images.unsplash.com/photo-1575936123452-b67c3203c357"
-        alt="hero"
-        className="mx-auto rounded-2xl shadow-2xl w-full md:w-2/3 object-cover"
-        initial={{ scale: 0.9, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 1 }}
-      />
+      <div>
+        <Accordion items={images} />
+      </div>
     </div>
   );
 }
